1eaa47c1e1186c1c600feb0177beeabb3a15208f
Ani Chisom <anichisom4top@gmail.com>
2026-05-22 06:22:57 +0000

    feat: allow temporary boot without DATABASE_URL via explicit flag

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,37 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { PrismaClient } = require("@prisma/client");
-
-const { PrismaPg } = require("@prisma/adapter-pg");
-
 const connectionString = process.env.DATABASE_URL;
+const allowNoDbBoot = process.env.ALLOW_START_WITHOUT_DB === "true";
 
-if (!connectionString) {
-  throw new Error("DATABASE_URL is required to initialize Prisma");
-}
+const makeMissingDbProxy = () =>
+  new Proxy(
+    {},
+    {
+      get() {
+        throw new Error(
+          "DATABASE_URL is not configured. Set DATABASE_URL or disable ALLOW_START_WITHOUT_DB."
+        );
+      },
+    }
+  );
 
-const adapter = new PrismaPg({ connectionString });
+const createPrismaClient = () => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { PrismaClient } = require("@prisma/client");
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { PrismaPg } = require("@prisma/adapter-pg");
 
-export const prisma = new PrismaClient({ adapter });
+  return new PrismaClient({
+    adapter: new PrismaPg({ connectionString }),
+  });
+};
+
+export const prisma = connectionString
+  ? createPrismaClient()
+  : allowNoDbBoot
+  ? makeMissingDbProxy()
+  : (() => {
+      throw new Error(
+        "DATABASE_URL is required to initialize Prisma. If you want temporary boot without DB, set ALLOW_START_WITHOUT_DB=true."
+      );
+    })();
+
+export const isDatabaseConfigured = Boolean(connectionString);
